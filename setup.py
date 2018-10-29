ef0f4e909af4764dd8f4dfaade3aaf3703bbc551
mikko.nieminen <mikko.nieminen@bihealth.de>
2018-10-29 15:22:38 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -11,12 +11,12 @@ with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name='sodar',
+    name='sodar_django_site',
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
     packages=find_packages(),
     include_package_data=True,
-    license='MIT License',
+    license='',
     description='Your SODAR Django Site (CHANGE ME)',
     long_description=README,
     url='https://bihealth.org',
@@ -27,7 +27,6 @@ setup(
         'Framework :: Django',
         'Framework :: Django :: 1.11',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
         'Operating System :: Linux',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3.5',
